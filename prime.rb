359851b41e7d53eb29482586dc0b6b1b06534cb5
Kitty Davies <kittyfdavies@gmail.com>
2018-03-20 00:25:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -11,18 +11,6 @@ def prime?(number)
       false
     elsif number % 7 == 0
       false
-    elsif number % 11 == 0
-      false
-    elsif number % 13 == 0
-      false
-    elsif number % 17 == 0
-      false
-    elsif number % 19 == 0
-      false
-    elsif number % 23 == 0
-      false
-    elsif number % 29 == 0
-      false
     else
       true
     end
