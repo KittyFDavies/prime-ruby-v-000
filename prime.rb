fcabfa4d6c8ed959a109ce1a2f55b8262ef078d8
Kitty Davies <kittyfdavies@gmail.com>
2018-03-20 00:20:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,7 @@
 # Add  code here!
 def prime?(number)
   if number > 3
-    if number % 2 == 0
-      false
-    elsif number % 3 == 0
-      false
-    elsif number % 5 == 0
-      false
-    elsif number % 7 == 0
+    if number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0 || number % 11 == 0 || number % 13 == 0 || number % 17 == 0 || number % 19 == 0 || number % 23 == 0 || number % 29 == 0
       false
     else
       true
